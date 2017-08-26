e483ed69f3903cd4f3581dac08c6d980da6f880a
Sam Pringle <15springle@gmail.com>
2017-08-25 20:25:29 -0700

    Change test command

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-until $(bash -c "docker-compose exec backend manage.py shell --command='pass' >/dev/null 2>/dev/null"); do
-    printf '[test script] waiting for backend container...'
+until $(bash -c "docker-compose exec backend python3 manage.py shell --command='pass' >/dev/null 2>/dev/null"); do
+    printf '[test script] waiting for backend container...\n'
     sleep 5
 done
 
-docker-compose exec backend python3 manage.py test
+docker-compose exec backend python3 manage.py check
